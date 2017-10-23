6520c1fc2b59c3d62b49bab39f348cabf1869c2f
Ben Jefferies <benjefferies@echosoft.uk>
2017-10-23 22:25:15 +0100

    Tidy up test

--- src/test/kotlin/taxi/rest/tests/PassengerTest.kt
+++ src/test/kotlin/taxi/rest/tests/PassengerTest.kt
@@ -5,12 +5,26 @@ import com.github.kittinunf.fuel.httpPost
 import io.restassured.RestAssured
 import io.restassured.RestAssured.given
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.After
 import org.junit.Test
 
 class PassengerTest {
 
     init {
         RestAssured.port = 4567
+    }
+
+    @After
+    fun clean() {
+        "http://localhost:9200/taxi/_delete_by_query?refresh=wait_for".httpPost().body("""
+            {
+                "query" : {
+                    "match_all": {}
+                }
+            }
+        """.trimIndent())
+            .authenticate("elastic", "changeme")
+            .response()
     }
 
     @Test
@@ -37,28 +51,8 @@ class PassengerTest {
 
     @Test
     fun shouldFindRideRequest() {
-        val response = """
-                            [
-                               {
-                                  "email":"test@example.com",
-                                  "lat":"10",
-                                  "lon":"12"
-                               },
-                               {
-                                  "email":"test@example.com",
-                                  "lat":"11",
-                                  "lon":"10"
-                               }
-                            ]
-                        """
         // Given
         // Passenger request ride
-        "http://localhost:4567/passenger/ride".httpPost().body("""
-            {
-                "lat": 10,
-                "lon": 10
-            }
-        """).response()
         "http://localhost:4567/passenger/ride".httpPost().body("""
             {
                 "lat": 10,
@@ -76,12 +70,43 @@ class PassengerTest {
                     .extract()
                     .body()
                     .jsonPath()
+        assertThat(body.getInt("$.size()")).isEqualTo(1)
         assertThat(body.getString("[0].email")).isEqualTo("test@example.com")
         assertThat(body.getString("[0].coordinates.lat")).isEqualTo("10.0")
         assertThat(body.getString("[0].coordinates.lon")).isEqualTo("10.0")
-        assertThat(body.getString("[1].email")).isEqualTo("test@example.com")
-        assertThat(body.getString("[1].coordinates.lat")).isEqualTo("10.0")
-        assertThat(body.getString("[1].coordinates.lon")).isEqualTo("10.0")
+    }
+
+    @Test
+    fun shouldLatestCoordinatesOfRideRequest() {
+        // Given
+        // Passenger request ride
+        "http://localhost:4567/passenger/ride".httpPost().body("""
+            {
+                "lat": 10,
+                "lon": 10
+            }
+        """).response()
+        "http://localhost:4567/passenger/ride".httpPost().body("""
+            {
+                "lat": 10.01,
+                "lon": 10.01
+            }
+        """).response()
+        val body = given()
+                    .queryParam("lat", 10)
+                    .queryParam("lon", 10)
+                .`when`()
+                    .get("/driver/ride")
+                .then()
+                    .assertThat()
+                    .statusCode(200)
+                    .extract()
+                    .body()
+                    .jsonPath()
+        assertThat(body.getInt("$.size()")).isEqualTo(1)
+        assertThat(body.getString("[0].email")).isEqualTo("test@example.com")
+        assertThat(body.getString("[0].coordinates.lat")).isEqualTo("10.01")
+        assertThat(body.getString("[0].coordinates.lon")).isEqualTo("10.01")
     }
 
     @Test
